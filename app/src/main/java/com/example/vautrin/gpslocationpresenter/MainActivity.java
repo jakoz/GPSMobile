26fe3073c7259f2d083f3c44e20975d67852e0b7
Jakub Kozlowski <jakub.kozlowski@gmx.com>
2017-09-10 20:47:31 +0200

    Map with multiple markers with addresses

--- app/src/main/java/com/example/vautrin/gpslocationpresenter/MainActivity.java
+++ app/src/main/java/com/example/vautrin/gpslocationpresenter/MainActivity.java
@@ -21,28 +21,24 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    Integer amountCoordinatesInt = 0;
+    String number = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ListView lv=(ListView)findViewById(R.id.listLastCoordinates);
-
-        EditText inputPhone = (EditText) findViewById(R.id.inputPhone);
-        String number = inputPhone.getText().toString();
-
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,fetchInbox(number));
-        lv.setAdapter(adapter);
+        readAllSms();
     }
 
     public ArrayList<String> fetchInbox(String number){
-
         ArrayList<String> sms=new ArrayList<String>();
         String[] phoneNumber = new String[] { number };
         Uri uriSms=Uri.parse("content://sms/inbox");
+        EditText amountCoordinates = (EditText) findViewById(R.id.inputAmountCoordinates);
+        amountCoordinatesInt = Integer.parseInt(amountCoordinates.getText().toString());
         Cursor cursor = getContentResolver().query(uriSms, new String[] { "_id", "thread_id", "address", "person", "date","body", "type" }, "address=?", phoneNumber, null);
         cursor.moveToFirst();
-        while(cursor.moveToNext()){
+        while(cursor.moveToNext() && amountCoordinatesInt > 0){
             String body=cursor.getString(cursor.getColumnIndexOrThrow("body"));
             body = body.replaceAll("\\r|\\n", "");
             if(body.contains("lat:")){
@@ -50,19 +46,43 @@ public class MainActivity extends AppCompatActivity {
                 int indexOfLongitude = body.indexOf("lon:")+4;
                 String latitude = body.substring(indexOfLatitude,indexOfLatitude+9);
                 String longitude = body.substring(indexOfLongitude,indexOfLongitude+9);
-                sms.add(latitude+"L"+longitude);
+                sms.add(latitude+"&"+longitude);
+                amountCoordinatesInt -= 1;
             }
         }
         return sms;
     }
 
     public void onGoToMapClick(View view){
-        TextView textView = (TextView) (findViewById(R.id.lblWelcome));
-        EditText inputPhone = (EditText) findViewById(R.id.inputPhone);
-        String number = inputPhone.getText().toString();
-
+        goToMap();
+    }
+    public void onGetNewClick(View view){
+        readAllSms();
+    }
+    public void onGetConfigWindow(View view){ showConfiguration();}
+    private void goToMap(){
         Intent intent = new Intent(this, MapsActivity.class);
         intent.putStringArrayListExtra("listView",fetchInbox(number));
-        startActivity(intent);
+        EditText amountCoordinates = (EditText) findViewById(R.id.inputAmountCoordinates);
+        intent.putExtra("amountCoordinatesInt",Integer.parseInt(amountCoordinates.getText().toString()));
+        ArrayList<String> listView = intent.getStringArrayListExtra("listView");
+        if (!listView.isEmpty()){
+            startActivity(intent);
+        }
+    }
+    private void readAllSms(){
+        ListView lv=(ListView)findViewById(R.id.listLastCoordinates);
+        //Amount of coordinates to read
+        EditText amountCoordinates = (EditText) findViewById(R.id.inputAmountCoordinates);
+        amountCoordinatesInt = Integer.parseInt(amountCoordinates.getText().toString());
+        //Phone number
+        EditText inputPhone = (EditText) findViewById(R.id.inputPhone);
+        number = inputPhone.getText().toString();
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,fetchInbox(number));
+        lv.setAdapter(adapter);
+    }
+    private void showConfiguration(){
+        Intent intent = new Intent(this, ConfigActivity.class);
     }
 }
--- app/src/main/java/com/example/vautrin/gpslocationpresenter/MapsActivity.java
+++ app/src/main/java/com/example/vautrin/gpslocationpresenter/MapsActivity.java
@@ -1,22 +1,39 @@
 package com.example.vautrin.gpslocationpresenter;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
 
+import com.google.android.gms.identity.intents.Address;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.*;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,26 +45,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
+            Intent intent = getIntent();
+            ArrayList<String> listView = intent.getStringArrayListExtra("listView");
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
 
-        Intent intent = getIntent();
-        ArrayList<String> listView = intent.getStringArrayListExtra("listView");
-        String firstCoordinates = listView.get(0);
-        String [] coordinates = firstCoordinates.split("L");
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(new Float(coordinates[0]), new Float(coordinates[1]));
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            for (int i = 0; i < listView.size(); i++) {
+                String coordinates = listView.get(i);
+                String address = "";
+                String[] coordinateAfterSplit = coordinates.split("&");
+                LatLng location = new LatLng(new Float(coordinateAfterSplit[0]), new Float(coordinateAfterSplit[1]));
+                Location loc = new Location("");
+                Geocoder geocoder = new Geocoder(this);
+                List<android.location.Address> addresses;
+                try {
+                    addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1);
+                    address = addresses.get(0).getThoroughfare() + " " + addresses.get(0).getSubThoroughfare();
+                }
+                catch(IOException e) {
+                }
+
+                mMap.addMarker(new MarkerOptions().position(location).title(address));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+                builder.include(location);
+            }
+            LatLngBounds bounds = builder.build();
+            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 20));
     }
+//    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                    return;
+//                }
+//
+//                loc.distanceBetween(location.latitude,location.longitude,locationCurrent.latitude,locationCurrent.longitude, results);
+
+    public String readLastSms(String number)
+    {
+        String sms=new String();
+        String[] phoneNumber = new String[] { number };
+        Uri uriSms=Uri.parse("content://sms/inbox");
+        Cursor cursor = getContentResolver().query(uriSms, new String[] { "_id", "thread_id", "address", "person", "date","body", "type" }, "address=?", phoneNumber, null);
+        cursor.moveToFirst();
+        String body=cursor.getString(cursor.getColumnIndexOrThrow("body"));
+        body = body.replaceAll("\\r|\\n", "");
+        if(body.contains("lat:")){
+            int indexOfLatitude = body.indexOf("lat:")+4;
+            int indexOfLongitude = body.indexOf("lon:")+4;
+            String latitude = body.substring(indexOfLatitude,indexOfLatitude+9);
+            String longitude = body.substring(indexOfLongitude,indexOfLongitude+9);
+            sms = latitude+"&"+longitude;
+        }
+        return sms;
+    }
+
+
 }
